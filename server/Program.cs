ac2d0a5d45ebf1c48a405107447e5763a976124c
higa <nikitagroshev11@gmail.com>
2026-01-23 00:37:11 +0300

    bug fix menu logic

--- server/Program.cs
+++ server/Program.cs
@@ -7,22 +7,26 @@ string? bio = "Грошев Никита Андреевич";
 string? group = "ИСП-232";
 string? data = "22.01.2026 00:20:56";
 
-string? input = Console.ReadLine();
-
 Console.WriteLine("Меню:");
 Console.WriteLine("1 — Показать ФИО");
 Console.WriteLine("2 — Показать группу");
 Console.WriteLine("3 — Показать дату");
 Console.WriteLine("4 — Выход");
+
+string? input = Console.ReadLine();
+
 switch (input)
 {
-    case 1:
+    case "1":
         Console.WriteLine(bio);
-    case 2:
+        break;
+    case "2":
         Console.WriteLine(group);
-    case 3:
+        break;
+    case "3":
         Console.WriteLine(data);
-    case 4:
+        break;
+    case "4":
         Console.WriteLine("Выход...");
         break;
 }
--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d17cd7c6d212d616354e13553bc5ba025dfe7053")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6ceb096e9668e19f2b40f847a4c6d2cadfdc1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

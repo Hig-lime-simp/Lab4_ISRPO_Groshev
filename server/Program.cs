9a40cfccb51691fdb6ec7f1b370c4dbac5879eb7
higa <nikitagroshev11@gmail.com>
2026-01-23 00:29:55 +0300

    Add menu

--- server/Program.cs
+++ server/Program.cs
@@ -7,4 +7,12 @@ Console.WriteLine("Я Грошев Никита Андреевич");
 string? group = "ИСП-232";
 string? data = "22.01.2026 00:20:56";
 
+string? input = Console.ReadLine();
 
+switch (input)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+}
